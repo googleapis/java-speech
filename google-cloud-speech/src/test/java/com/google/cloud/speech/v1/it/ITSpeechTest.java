09546eca7b832d990f2b75ed7008a7360a9dce77
Jeff Ching <chingor@google.com>
2020-08-14 11:03:35 -0700

    chore: fix lint

--- google-cloud-speech/src/test/java/com/google/cloud/speech/v1/it/ITSpeechTest.java
+++ google-cloud-speech/src/test/java/com/google/cloud/speech/v1/it/ITSpeechTest.java
@@ -151,6 +151,8 @@ public class ITSpeechTest {
     if (audio_gcs_uri != null) {
       return RecognitionAudio.newBuilder().setUri(audio_gcs_uri).build();
     }
-    return RecognitionAudio.newBuilder().setUri("gs://cloud-samples-data/speech/hello.flac").build();
+    return RecognitionAudio.newBuilder()
+        .setUri("gs://cloud-samples-data/speech/hello.flac")
+        .build();
   }
 }
